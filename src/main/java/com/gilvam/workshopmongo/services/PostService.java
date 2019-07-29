48c51cbc1cb04c93d7f53cb9957f83526647e01f
Gilvam Mourão <gilvamm@gmail.com>
2019-07-29 09:06:38 -0300

    projeto final

---
+++ src/main/java/com/gilvam/workshopmongo/services/PostService.java
@@ -0,0 +1,32 @@
+package com.gilvam.workshopmongo.services;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import com.gilvam.workshopmongo.domain.Post;
+import com.gilvam.workshopmongo.repository.PostRepository;
+import com.gilvam.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
+}
